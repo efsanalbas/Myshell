084eb367a5899cb8c222c3031da3e87a9c2d5c42
efsanalbas <115089520+efsanalbas@users.noreply.github.com>
2022-11-24 23:19:35 +0300

    Update writef.c

--- writef.c
+++ writef.c
@@ -13,6 +13,44 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <time.h>
+void reverse(char s[])
+{
+    int i, j;
+    char c;
+    for (i = 0, j = strlen(s)-1; i<j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
+char * itoa(int n, char s[])
+ {
+     int i, sign;
+ 
+     if ((sign = n) < 0)  /* record sign */
+         n = -n;          /* make n positive */
+     i = 0;
+     do {       /* generate digits in reverse order */
+         s[i++] = n % 10 + '0';   /* get next digit */
+     } while ((n /= 10) > 0);     /* delete it */
+     if (sign < 0)
+         s[i++] = '-';
+     s[i] = '\0';
+    reverse(s);
+    return s;
+ }
+void printValues(FILE* in_file){
+    char pid[10];
+    char ppid[10];
+    time_t a = time(NULL);
+    char * time = ctime(&a);
+    time[strlen(time)-1] = '\0';
+    fprintf(in_file, "%s", "\n");
+    fprintf(in_file, "Pid:%s, Parent Pid:%s,  System Time:%s", itoa (getpid(),pid),itoa (getppid(),ppid), time);
+    fprintf(in_file, "%s", "\n");
+
+}
 
 void writef(char const filename[20],char *buffer){
     FILE *in_file ;
@@ -24,14 +62,16 @@ void writef(char const filename[20],char *buffer){
         if (in_file == NULL){
             in_file = fopen(filename, "w");//Baştan dosya oluştururum.
             fprintf(in_file, "%s", buffer);
-            printf("Dosyaya yazıldı.\n");
+            fprintf(in_file, "%s", " ");
+            printValues(in_file);
             fclose(in_file);
         }
         
         else {//in_file null değilse dosya vardır ve append modunda açılır.
             FILE *in_file = fopen(filename, "a");
             fprintf(in_file, "%s", buffer);
-            printf("Dosyaya yazıldı.\n");
+            fprintf(in_file, "%s", " ");
+            printValues(in_file);
             fclose(in_file);
         }
     }
@@ -40,35 +80,41 @@ void writef2(char const filename[20]){
     FILE *in_file ;
     if(in_file != NULL){
     in_file = fopen(filename, "r");//Baştan dosya oluştururum.
+        fprintf(in_file, "%s", " ");
+        printValues(in_file);
+
     }
     else{
     in_file = fopen(filename, "r");
+        fprintf(in_file, "%s", " ");
+        printValues(in_file);
     }
     
 }
 
 int main(int argc, char *argv[]){
-    if(argc <2){
-        writef2(argv[1]);
+    if(argc <3){
+        writef2(argv[2]);
     }
     else{
         char *message = malloc(sizeof(char) * 200);
         
-        int i = 2;
+        int i = 3;
         int j = 0;
         char *ch;
         while(i < argc){
             while(*(argv + i) != '\0'){
                 ch = *(argv+i);
-                for(int j=0; j<sizeof(ch)/sizeof(ch[0]); j++){
+                for(int j=0; j<strlen(ch); j++){
                     message[j] = ch[j];
                 }
                 i++;
             }
         }
-        writef(argv[1], message);
+        writef(argv[2], message);
     }
     return 0;
 }
+
 
 
