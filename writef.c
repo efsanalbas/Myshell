ffad03e98a282f18bb6918dee804add0b376e858
efsanalbas <115089520+efsanalbas@users.noreply.github.com>
2022-11-28 23:16:29 +0300

    Update writef.c

--- writef.c
+++ writef.c
@@ -15,40 +15,38 @@
 #include <sys/stat.h>
 #include <time.h>
 
-void reverse(char s[])
+char *convert(int number)
 {
-    int i, j;
-    char c;
-    for (i = 0, j = strlen(s)-1; i<j; i++, j--) {
-        c = s[i];
-        s[i] = s[j];
-        s[j] = c;
+    int count=0;
+    int numbertmp=number;
+    while(numbertmp>0)
+    {
+        int digit= numbertmp%10;
+        numbertmp/=10;
+        count++;
     }
+    char * value= (char * ) calloc (count,sizeof(unsigned char)  );
+    numbertmp=number;
+    count--;
+    while(numbertmp>0)
+    {
+        int digit= numbertmp%10;
+        numbertmp/=10;
+        char x=(char) 48+digit;
+        *(value+count)=x;
+        count--;
+    }
+
+    return value;
 }
-char * itoa(int n, char s[])
- {
-     int i, sign;
- 
-     if ((sign = n) < 0)  /* record sign */
-         n = -n;          /* make n positive */
-     i = 0;
-     do {       /* generate digits in reverse order */
-         s[i++] = n % 10 + '0';   /* get next digit */
-     } while ((n /= 10) > 0);     /* delete it */
-     if (sign < 0)
-         s[i++] = '-';
-     s[i] = '\0';
-    reverse(s);
-    return s;
- }
 void printValues(FILE* in_file){
-    char pid[10];
-    char ppid[10];
+    char *pid = convert(getpid());
+    char *ppid = convert(getppid());
     time_t a = time(NULL);
     char * time = ctime(&a);
     time[strlen(time)-1] = '\0';
     fprintf(in_file, "%s", "\n");
-    fprintf(in_file, "Pid:%s, Parent Pid:%s,  System Time:%s", itoa (getpid(),pid),itoa (getppid(),ppid), time);
+    fprintf(in_file, "Pid:%s, Parent Pid:%s,  System Time:%s", pid,ppid, time);
     fprintf(in_file, "%s", "\n");
 
 }
@@ -57,18 +55,18 @@ void writef(char const filename[20],char *buffer){
     FILE *in_file ;
     if(buffer == NULL){
         if(in_file != NULL)
-        in_file = fopen(filename, "r");//Okunacak dosyayı bulamazsa dosya yoktur.
+        in_file = fopen(filename, "r");//Okunacak dosyay\u0131 bulamazsa dosya yoktur.
     }
     else{
         if (in_file == NULL){
-            in_file = fopen(filename, "w");//Baştan dosya oluştururum.
+            in_file = fopen(filename, "w");//Ba\u015ftan dosya olu\u015ftururum.
             fprintf(in_file, "%s", buffer);
             fprintf(in_file, "%s", " ");
             printValues(in_file);
             fclose(in_file);
         }
         
-        else {//in_file null değilse dosya vardır ve append modunda açılır.
+        else {//in_file null de\u011filse dosya vard\u0131r ve append modunda a\u00e7\u0131l\u0131r.
             FILE *in_file = fopen(filename, "a");
             fprintf(in_file, "%s", buffer);
             fprintf(in_file, "%s", " ");
@@ -77,45 +75,25 @@ void writef(char const filename[20],char *buffer){
         }
     }
 }
-void writef2(char const filename[20]){
-    FILE *in_file ;
-    if(in_file != NULL){
-    in_file = fopen(filename, "r");//Baştan dosya oluştururum.
-        fprintf(in_file, "%s", " ");
-        printValues(in_file);
 
-    }
-    else{
-    in_file = fopen(filename, "r");
-        fprintf(in_file, "%s", " ");
-        printValues(in_file);
-    }
-    
-}
 
 int main(int argc, char *argv[]){
-    if(argc <3){
-        writef2(argv[2]);
-    }
-    else{
-        char *message = malloc(sizeof(char) * 200);
-        
-        int i = 3;
-        int j = 0;
-        char *ch;
-        while(i < argc){
-            while(*(argv + i) != '\0'){
-                ch = *(argv+i);
-                for(int j=0; j<strlen(ch); j++){
-                    message[j] = ch[j];
-                }
-                i++;
-            }
-        }
-        writef(argv[2], message);
-    }
+
+        char * message =malloc(sizeof(char) * 200);
+
+          for(int i = 3; i < argc; i++) {
+              strcat(message, argv[i]);
+              strcat(message, " ");
+          }
+
+          printf("%s\n", message);
+          writef(argv[2], message);
+    
     return 0;
 }
+
+
+
 
 
  
