2fcfabb9adc07608bd366dab4e40635fd7b6c575
efsanalbas <115089520+efsanalbas@users.noreply.github.com>
2022-11-29 18:28:39 +0300

    Final commit

--- writef.c
+++ writef.c
@@ -5,7 +5,6 @@
 //  Created by Nur Efsan Albas on 19.11.2022.
 //
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,85 +14,84 @@
 #include <sys/stat.h>
 #include <time.h>
 
-char *convert(int number)
+char *convert(int number) // number stringe donusturuldu.
 {
-    int count=0;
-    int numbertmp=number;
-    while(numbertmp>0)
+    int count = 0;
+    int numbertmp = number;
+    while (numbertmp > 0)
     {
-        int digit= numbertmp%10;
-        numbertmp/=10;
+        int digit = numbertmp % 10;
+        numbertmp /= 10;
         count++;
     }
-    char * value= (char * ) calloc (count,sizeof(unsigned char)  );
-    numbertmp=number;
+    char *value = (char *)calloc(count, sizeof(unsigned char));
+    numbertmp = number;
     count--;
-    while(numbertmp>0)
+    while (numbertmp > 0)
     {
-        int digit= numbertmp%10;
-        numbertmp/=10;
-        char x=(char) 48+digit;
-        *(value+count)=x;
+        int digit = numbertmp % 10;
+        numbertmp /= 10;
+        char x = (char)48 + digit;
+        *(value + count) = x;
         count--;
     }
 
     return value;
 }
-void printValues(FILE* in_file){
-    char *pid = convert(getpid());
-    char *ppid = convert(getppid());
+void printValues(FILE *in_file)      // dosya uzerinde islem yapildiginda icerisine yazdirilacak degerler. (pid, ppid, sistem saati gibi)
+{
+    char *pid = convert(getpid());   // int deger stringe donusturuldu.
+    char *ppid = convert(getppid()); // int deger stringe donusturuldu
     time_t a = time(NULL);
-    char * time = ctime(&a);
-    time[strlen(time)-1] = '\0';
+    char *time = ctime(&a);
+    time[strlen(time) - 1] = '\0';
     fprintf(in_file, "%s", "\n");
-    fprintf(in_file, "Pid:%s, Parent Pid:%s,  System Time:%s", pid,ppid, time);
+    fprintf(in_file, "Pid:%s, Parent Pid:%s,  System Time:%s", pid, ppid, time);
     fprintf(in_file, "%s", "\n");
-
+}
+void writetoFile(FILE *in_file,char *buffer){  //argumanlari dosyaya yazar.
+    fprintf(in_file, "%s", buffer); //Buffer'in icine yazar.
+    fprintf(in_file, "%s", " ");    //Bosluklarini ekler.
+    printValues(in_file);          //pid,ppid,sistem saatini yazdirir.
+    fclose(in_file);               //Dosyayi kapatir.
+}
+void writef(char const filename[20], char *buffer)
+{
+    FILE *in_file;
+    if (buffer == NULL) //Kullanici dosya icine birsey yazdırmak istemesse -buffer null ise- bu kod satiri calisir.
+    {
+        if (in_file != NULL)                  //dosya vardır
+            in_file = fopen(filename, "r"); // Okunacak dosyayi bulamazsa dosya yoktur.
+        else
+            in_file = fopen(filename, "w"); //dosya yoksa bastan olusturur.
+    }
+    else
+    {
+        if (in_file == NULL)    //dosya yoksa, daha onceden olusturulmadiysa
+        {
+            in_file = fopen(filename, "w"); // Bastan dosya olusturur.
+            writetoFile(in_file,buffer);
+        }
+        else
+        { // in_file null degilse dosya vardır ve append modunda acilir.
+            FILE *in_file = fopen(filename, "a"); //dosya varsa uzerine ekleme yapar.
+            writetoFile(in_file,buffer);
+        }
+    }
 }
 
-void writef(char const filename[20],char *buffer){
-    FILE *in_file ;
-    if(buffer == NULL){
-        if(in_file != NULL)
-        in_file = fopen(filename, "r");//Okunacak dosyay\u0131 bulamazsa dosya yoktur.
+int main(int argc, char *argv[])
+{
+
+    char *message = malloc(sizeof(char) * 200); //kullanicinin gonderecegi mesaji eklemek icin bir mesaj arrayi olusturdum.
+
+    for (int i = 3; i < argc; i++) // 3.komuttan itibaren kullanici yazmaya basladigi icin mesajlar ucuncu komuttan itibaren                                         birlestirilir.
+    {
+        strcat(message, argv[i]); //satirlari birlestirir.
+        strcat(message, " ");     //bosluklarini ekler.
     }
-    else{
-        if (in_file == NULL){
-            in_file = fopen(filename, "w");//Ba\u015ftan dosya olu\u015ftururum.
-            fprintf(in_file, "%s", buffer);
-            fprintf(in_file, "%s", " ");
-            printValues(in_file);
-            fclose(in_file);
-        }
-        
-        else {//in_file null de\u011filse dosya vard\u0131r ve append modunda a\u00e7\u0131l\u0131r.
-            FILE *in_file = fopen(filename, "a");
-            fprintf(in_file, "%s", buffer);
-            fprintf(in_file, "%s", " ");
-            printValues(in_file);
-            fclose(in_file);
-        }
-    }
-}
+    writef(argv[2], message);    //mesaji writefe gönderir.
 
-
-int main(int argc, char *argv[]){
-
-        char * message =malloc(sizeof(char) * 200);
-
-          for(int i = 3; i < argc; i++) {
-              strcat(message, argv[i]);
-              strcat(message, " ");
-          }
-
-          printf("%s\n", message);
-          writef(argv[2], message);
-    
     return 0;
 }
 
-
-
-
-
- 
